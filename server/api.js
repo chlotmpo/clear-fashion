d056ecaab4d624ea7062e187ac175677deb953b1
Chloé TEMPO <76529865+chlotmpo@users.noreply.github.com>
2022-03-13 18:30:30 +0100

    Correction on the search endpoint. Now completely fonctionnal with all filters type

--- server/api.js
+++ server/api.js
@@ -33,28 +33,42 @@ app.get('/products', async(request, response) => {
   response.send({"products" : products});
 })
 
-// Products by id
-// app.get('/products/:_id', async(request, response) => {
-//   products = await db.findProducts({'_id': new ObjectId(request.params._id)}, false)
-//   response.send({"count" : products.length, "products" : products});
-// })
-
 
 // Product search, Search for specific product 
 app.get('/products/search', async(request, response) => {
-  products = await db.findAllProducts(false)
   const filters = request.query;
-  const filteredProducts = await products.filter(product => {
-    let isValid = true;
-    for (key in filters) {
-      console.log(key, product[key], filters[key]);
-      isValid = isValid && product[key] == filters[key];
-    }
-    return isValid;
-  });
+  console.log('filters :>> ', filters);
+  
+  const brand = filters.brand !== undefined ? filters.brand : ''
+  const price = parseInt(filters.price,10) > 0 ? parseInt(filters.price,10) : ''
+  const limit = parseInt(filters.limit,10) > 0 ? parseInt(filters.limit,10) : 12
+
+  var match = {}
+  if( brand === '' &&  price !== '') match = {price: price} 
+  else if(brand !== '' && price === '') match = {brand: brand}
+  else if(brand !== '' && price !== '') match = {brand: brand, price: price}
+
+  query = [
+    {'$match' : match},
+    {'$sort' : {price:1}},
+    {'$limit' : limit}
+    ]
+  console.log('query :>> ', query);
+  
+  var filteredProducts = await db.aggregateQuery(query)
+
   console.log('filteredProducts.length :>> ', filteredProducts.length);
   response.send(filteredProducts);
 })
+
+
+// Products by id
+app.get('/products/:_id',  async(request, response) => {
+  products = await db.findProducts({'_id': new ObjectId(request.params._id)}, false)
+  response.send({"count" : products.length, "products" : products});
+})
+
+
 // Products by brand 
 app.get('/products/brand=', async(request, response) => {
   products = await db.findProducts({'brand': 'adresse'}, false)
