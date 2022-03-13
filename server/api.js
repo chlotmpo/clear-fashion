daa97650e40fb80ac69246ea0e34f501aeb063f2
Chloé TEMPO <76529865+chlotmpo@users.noreply.github.com>
2022-03-13 17:11:30 +0100

    Search endpoint with detection of the different filters

--- server/api.js
+++ server/api.js
@@ -34,11 +34,27 @@ app.get('/products', async(request, response) => {
 })
 
 // Products by id
-app.get('/products/:_id', async(request, response) => {
-  products = await db.findProducts({'_id': new ObjectId(request.params._id)}, false)
-  response.send({"count" : products.length, "products" : products});
-})
+// app.get('/products/:_id', async(request, response) => {
+//   products = await db.findProducts({'_id': new ObjectId(request.params._id)}, false)
+//   response.send({"count" : products.length, "products" : products});
+// })
 
+
+// Product search, Search for specific product 
+app.get('/products/search', async(request, response) => {
+  products = await db.findAllProducts(false)
+  const filters = request.query;
+  const filteredProducts = await products.filter(product => {
+    let isValid = true;
+    for (key in filters) {
+      console.log(key, product[key], filters[key]);
+      isValid = isValid && product[key] == filters[key];
+    }
+    return isValid;
+  });
+  console.log('filteredProducts.length :>> ', filteredProducts.length);
+  response.send(filteredProducts);
+})
 // Products by brand 
 app.get('/products/brand=', async(request, response) => {
   products = await db.findProducts({'brand': 'adresse'}, false)
